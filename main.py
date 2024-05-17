4cd68aa398ed39e777379f89f5068c294b115682
Kenn-Michael Sanga <kennsanga@gmail.com>
2024-05-17 17:24:44 +0200

    created addEdge function with hardcoded test data (trainline not yet implemented)

--- main.py
+++ main.py
@@ -1,39 +1,85 @@
-class Vertex:
-    pass
+class Node:
+    def __init__(self, next=None):
+        self.next = next
+
+    def set_next(self, new_next_node):
+        self.next = new_next_node
 
 
-class AdjListNode:
-    def __init__(self, value: Vertex):
-        self.value = value
-        self.next = None
-
-class Vertex:
-    def __init__(self, station_name, trainline):
+class Vertex(Node):
+    def __init__(self, station_name: str, next=None):
+        super().__init__(next)
         self.station_name = station_name
-        self.trainline = trainline
-class Edge:
-    def __init__(self, destination: Vertex, traveltime):
-        self.destination = destination.station_name
-        self.trainline = destination.trainline
-        self.traveltime = traveltime # Weight
+        self.neighbours = LinkedList()  # linked list of edges
+
+
+class Edge(Node):
+    def __init__(self, neighbour: Vertex, traveltime: int, next=None):
+        super().__init__(next)
+        self.neighbour_station_name = neighbour.station_name
+        self.traveltime = traveltime  # weight
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def add(self, value):
+        newNode = value
+        newNode.set_next(self.head)
+        self.head = newNode
+
+    def __iter__(self):
+        current = self.head
+        while current:
+            yield current
+            current = current.next
+
+
 class Graph:
-    def __init__(self, verticesNum: int):
-        self.verticesNum = verticesNum
-        self.graph = [None]  * self.verticesNum
+    def __init__(self):
+        self.vertices = {}  # dictionary of vertices by station name (key=station name, value=vertex object)
 
-    def addVertex(self, source: Vertex, destination: Vertex):
-        newVertex = AdjListNode(destination)
-        newVertex.next = self.graph[source]
-        self.graph[source] = newVertex
+    def get_vertex(self, station_name):
+        return self.vertices.get(station_name)
 
+    def add_edge(self, first_station: str, second_station: str, weight: int):
+        # Create vertex object and store in dictionary of vertices, if it's not in dictionary already
+        if first_station not in self.vertices:
+            self.vertices[first_station] = Vertex(first_station)
+        if second_station not in self.vertices:
+            self.vertices[second_station] = Vertex(second_station)
 
+        # Access vertex objects of first and second station
+        first_vertex = self.vertices[first_station]
+        second_vertex = self.vertices[second_station]
 
-def dijkstra(graph: Graph, nodesNum: int, startNode: Vertex):
+        # Create edges
+        first_edge = Edge(second_vertex, weight)
+        second_edge = Edge(first_vertex, weight)
+
+        # Add edges to vertices' list of edges
+        first_vertex.neighbours.add(first_edge)
+        second_vertex.neighbours.add(second_edge)
+
+    def __iter__(self):
+        return iter(self.vertices.values())
+
 
 def main():
     ubahnnetz = Graph()
+    ubahnnetz.add_edge("Leopoldau", "Grossfeldsiedlung", 2)
+    ubahnnetz.add_edge("Grossfeldsiedlung", "Aderklaaer Strasse", 1)
+    ubahnnetz.add_edge("Aderklaaer Strasse", "Rennbahnweg", 1)
+    ubahnnetz.add_edge("Rennbahnweg", "Kagraner Platz", 2)
 
+    # Example of iterating over the graph
+    for vertex in ubahnnetz:
+        print(f"Station: {vertex.station_name}")
+        for edge in vertex.neighbours:
+            print(f"  connects to {edge.neighbour_station_name} with travel time {edge.traveltime}")
+
+    print(ubahnnetz.vertices["Leopoldau"].neighbours.head.neighbour_station_name)
 
 if __name__ == '__main__':
     main()
-
